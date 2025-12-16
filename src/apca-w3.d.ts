21935c70e95ef1199354db94d0ed20fec0910d19
Cody Fitzgerald <Cody.Fitzgerald@docusign.com>
2025-12-15 21:42:10 -0800

    Updates

--- src/apca-w3.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'apca-w3' {
-  export function APCAcontrast(textColor: string, bgColor: string): number;
-  export function sRGBtoY(color: string): number;
-  export function displayP3toY(color: string): number;
-}
